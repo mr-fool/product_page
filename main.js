7cf17c9f41ced699d84f85b31abf36124ce4430d
mr-fool <mr-fool@users.noreply.github.com>
2019-12-20 15:30:59 -0700

    variant issues

--- main.js
+++ main.js
@@ -2,6 +2,21 @@ var app = new Vue({
 	el: "#app",
 	data: {
 	  product: 'Adidas',
-	  image: "./logo.png"
-  }
+	  image: "./logo.png",
+	  inventory: 11,
+	  details: ["US size 8", "gopnik style", "male"]
+	},
+
+	variants: [
+	{
+
+		variantId: 2234,
+		variantColor: "green"
+	},
+	{
+		variantId: 2235,
+		variantColor: "blue"	
+	}
+
+	]  
 })
