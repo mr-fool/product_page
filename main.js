aa0557fefbd47c17f107cd9e1624cd3b9d1d1255
mr-fool <mr-fool@users.noreply.github.com>
2019-12-20 16:05:42 -0700

    variants should be inside data

--- main.js
+++ main.js
@@ -4,19 +4,18 @@ var app = new Vue({
 	  product: 'Adidas',
 	  image: "./logo.png",
 	  inventory: 11,
-	  details: ["US size 8", "gopnik style", "male"]
-	},
-
-	variants: [
-	{
-
-		variantId: 2234,
-		variantColor: "green"
-	},
-	{
-		variantId: 2235,
-		variantColor: "blue"	
+	  details: ["US size 8", "gopnik style", "male"],
+	  variants: [
+		{
+	
+			variantId: 2234,
+			variantColor: "white"
+		},
+		{
+			variantId: 2235,
+			variantColor: "black"	
+		}
+	
+		]  
 	}
-
-	]  
 })
